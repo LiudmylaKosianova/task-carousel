8c3ab5ca1debce84e0eb8917747f2372e2f45fd0
Blueberry <snw.wht.lk@gmail.com>
2023-03-26 13:48:40 +0200

    CompleteByRequestTask

--- src/main/java/com/epam/rd/autotasks/CompleteByRequestTask.java
+++ src/main/java/com/epam/rd/autotasks/CompleteByRequestTask.java
@@ -1,7 +1,6 @@
 package com.epam.rd.autotasks;
 
 /**
- * - Constructor of the `CompleteByRequestTask` takes no parameters.
  * - Calling `execute` method on the task does not make it finished until the `complete` method is called.
  * - Once the `complete` method is called, the next call to `execute` makes the task finished.
  * Note that the task is not finished right after calling the `complete` method.
@@ -9,18 +8,25 @@ package com.epam.rd.autotasks;
  */
 
 public class CompleteByRequestTask implements Task {
+    public boolean completeWasCalled, executeWasCalled = false;
+    public CompleteByRequestTask() {
+    }
 
     @Override
     public void execute() {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        executeWasCalled = true;
+
     }
 
     @Override
     public boolean isFinished() {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        return completeWasCalled && executeWasCalled;
     }
 
     public void complete() {
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+        completeWasCalled = true;
     }
 }
